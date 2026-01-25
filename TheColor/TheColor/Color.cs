95ca07883773e7cabddfb9bd0e6eedd5508a5c9e
HarryColyer <harrycolyer@hotmail.co.uk>
2026-01-25 18:59:42 +0000

    Challenge complete

---
+++ TheColor/TheColor/Color.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheColor
+{
+    internal class Color
+    {
+        public byte RedValue { get; private set; }
+        public byte GreenValue { get; private set; }
+        public byte BlueValue { get; private set; }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            RedValue = red;
+            GreenValue = green;
+            BlueValue = blue;
+        }
+
+        public static Color Red
+        {
+            get
+            {
+                return new Color(255, 0, 0);
+            }
+        }
+
+        public static Color Green
+        {
+            get
+            {
+                return new Color(0, 255, 0);
+            }
+        }
+
+        public static Color Blue
+        {
+            get
+            {
+                return new Color(0, 0, 255);
+            }
+        }
+
+        public static Color Purple
+        {
+            get
+            {
+                return new Color(128, 0 , 128);
+            }
+        }
+
+        public static Color Yellow
+        {
+            get
+            {
+                return new Color(255, 255, 0);
+            }
+        }
+
+        public static Color Orange
+        {
+            get
+            {
+                return new Color(255, 165, 0);
+            }
+        }
+
+        public static Color Black
+        {
+            get
+            {
+                return new Color(0, 0, 0);
+            }
+        }
+
+        public static Color White
+        {
+            get
+            {
+                return new Color(255, 255, 255);
+            }
+        }
+    }
+}
---
+++ TheColor/TheColor/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TheColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Color blackColor = new (0, 0, 0);
+            Color whiteColor = Color.White;
+
+            Console.WriteLine($"The RGB values for black are ({blackColor.RedValue}, {blackColor.GreenValue}, {blackColor.BlueValue})");
+            Console.WriteLine($"The RGB values for white are ({whiteColor.RedValue}, {whiteColor.GreenValue}, {whiteColor.BlueValue})");
+            Console.ReadKey();
+        }
+    }
+}
