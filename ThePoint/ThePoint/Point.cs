2062161fa4e7523aa72ea92f4b763c8228a97f9c
HarryColyer <harrycolyer@hotmail.co.uk>
2026-01-25 18:59:32 +0000

    Challenge complete

---
+++ ThePoint/ThePoint/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePoint
+{
+    internal class Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ ThePoint/ThePoint/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ThePoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pointOne = new (2, 3);
+            Point pointTwo = new (-4, 0);
+
+            Console.WriteLine($"({pointOne.X}, {pointOne.Y})");
+            Console.WriteLine($"({pointTwo.X}, {pointTwo.Y})");
+            Console.ReadKey();
+        }
+    }
+}
