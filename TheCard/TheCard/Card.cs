05726f7bb59dfe87d7a9b4b422b378f5ffca24b1
HarryColyer <harrycolyer@hotmail.co.uk>
2026-01-26 10:29:19 +0000

    Challenge Complete

---
+++ TheCard/TheCard/Card.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCard
+{
+    enum CardColor { Red, Green, Blue, Yellow }
+    enum CardRank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Modulo, Caret, Ampersand }
+    internal class Card
+    {
+        public CardRank Rank { get; private set; }
+
+        public CardColor Color { get; private set; }
+
+        public Card(CardColor color, CardRank rank)
+        {
+            Rank = rank;
+            Color = color;
+        }
+
+        public bool IsCardNumber()
+        {
+            return Rank <= CardRank.Ten;
+        }
+
+        public override string ToString()
+        {
+            return $"The {Color} {Rank}";
+        }
+    }
+}
---
+++ TheCard/TheCard/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TheCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The digital Realms of C# have playing cards like ours but with some differences. Each card has a color
+            //(red, green, blue, yellow) and a rank(the numbers 1 through 10, followed by the symbols $, %, ^, and &).
+            //The third pedestal requires that you create a class to represent a card of this nature.
+            //        Objectives:
+            //• Define enumerations for card colors and card ranks.
+            //• Define a Card class to represent a card with a color and a rank, as described above.
+            //• Add properties or methods that tell you if a card is a number or symbol card (the equivalent of a
+            //face card).
+            //• Create a main method that will create a card instance for the whole deck(every color with every
+            //rank) and display each(for example, “The Red Ampersand” and “The Blue Seven”).
+            //• Answer this question: Why do you think we used a color enumeration here but made a color class
+            //in the previous challenge?
+
+
+            static void CreateDeck()
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 14; j++)
+                    {
+                        Card card = new Card((CardColor)i, (CardRank)j);
+                        Console.WriteLine(card.ToString());
+                    }
+                }
+
+            }
+            CreateDeck();
+            Console.ReadKey();
+        }
+    }
+}
